31b47158fee0a04e670ecd89595d24412499122b
sarahob96 <sarahobrien15@gmail.com>
2022-10-04 21:09:15 +0000

    create the product model within the product app and migrate

--- products/models.py
+++ products/models.py
@@ -12,3 +12,18 @@ class Category(models.Model):
     
     def get_category_name(self):
         return self.category_name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    sku = models.CharField(max_length=200)
+    product_description = models.TextField()
+    rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image_url = models.URLField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
