4ec0e0af924329aeb07faff1e1d4ae5ff76262e6
sarahob96 <sarahobrien15@gmail.com>
2022-10-06 18:53:40 +0000

    Create basic view to display products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def products(request):
+
+    items = Product.objects.all()
+
+    return render(request, 'products/products.html', {'items': items})
