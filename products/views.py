616ef9d93a81b9bcfb48579496b1646df6b3dcbc
sarahob96 <sarahobrien15@gmail.com>
2022-10-08 18:58:11 +0000

    create a view for product information and add to urls.py. Create a prod info template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -12,3 +12,13 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+def product_info(request, product_id):
+
+    item = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': item,
+    }
+
+    return render(request, 'products/product_info.html', context)
