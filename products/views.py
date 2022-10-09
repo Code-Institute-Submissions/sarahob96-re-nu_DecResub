b57d14e03a5876dc560ab06e6d46e306619c396b
sarahob96 <sarahobrien15@gmail.com>
2022-10-09 19:51:12 +0000

    add the filter by feature to the product page and use js to give functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 # Create your views here.
 
@@ -10,6 +11,23 @@ def all_products(request):
     products = Product.objects.all()
     search_query = None
     categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sorting = request.GET['sort']
+            sort = sorting
+            if sorting == 'name':
+                sorting= 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sorting == 'renu_category':
+                sorting = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'descending':
+                    sorting = f'-{sorting}'
+            products = products.order_by(sorting)
 
     if request.GET:
 
@@ -27,10 +45,13 @@ def all_products(request):
             search_queries = Q(name__icontains=search_query) | Q(product_description__icontains=search_query)
             products = products.filter(search_queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_queries': search_query,
         'active_category': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
