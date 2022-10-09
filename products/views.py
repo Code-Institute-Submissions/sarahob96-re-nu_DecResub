4bfa4ab567b211a4c00686f7fdab8c3b3aba14fe
sarahob96 <sarahobrien15@gmail.com>
2022-10-09 16:27:56 +0000

    add to all_product views to enable search querys

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,9 +8,21 @@ from .models import Product
 def all_products(request):
 
     products = Product.objects.all()
+    search_query = None
 
+    if request.GET:
+        if 's' in request.GET:
+            search_query = request.GET['s']
+            if not search_query:
+                message.error(request, 'please enter a search item')
+                return redirect(reverse('products'))
+            
+            search_queries = Q(name__icontains=search_query) | Q(product_description__icontains=search_query)
+            products = products.filter(search_queries)
+            
     context = {
         'products': products,
+        'search_queries': search_query
     }
 
     return render(request, 'products/products.html', context)
