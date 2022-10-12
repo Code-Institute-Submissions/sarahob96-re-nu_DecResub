115bd54f1a6121c1d1f0e1f2519a9f6258fee0a4
sarahob96 <sarahobrien15@gmail.com>
2022-10-12 21:02:50 +0000

    alter layout of form element on thhe pr prod_info.html

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
 
 # Create your views here.
+
 
 def shopping_bag(request):
 
     return render(request, 'bag/bag.html')
+
