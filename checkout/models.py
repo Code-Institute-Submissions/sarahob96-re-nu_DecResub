a76522e661def245842bbea4eaed8c525b2a6594
sarahob96 <sarahobrien15@gmail.com>
2022-10-16 16:41:21 +0000

    create an order number generator function and assign an order number if one is not created

--- checkout/models.py
+++ checkout/models.py
@@ -1,8 +1,14 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import Settings
+from django_countries.fields import CountryField
+
+from products.models import Product
 
 # Create your models here.
 
-class Purchase(models.Models):
+class Checkout(models.Models):
 
     order_number = models.CharField(max_length=20, null=False, editable=False)
     date = models.DateTimeField(auto_now_add=True)
@@ -13,8 +19,25 @@ class Purchase(models.Models):
     address_line_2 = models.CharField(max_length=100, null=True, blank=True)
     town = models.CharField(max_length=40, null=False, blank=False)
     city = models.CharField(max_length=40, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="country", null=False, blank=False)
     postcode = models.CharField(max_length=40, null=False, blank=False)
     delivery = models.DecimalField(max_digits=8, null=False, default=0)
     order_total = models.DecimalField(max_digits=12, null=False, default=0)
     grand_total = models.DecimalField(max_digits=12, null=False, default=0)
+
+    def create_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+
+        if not self.order_number:
+            self.order.number = self.create_order_number()
+        super().save(*args, **kwargs)
+
+    def update_total_cost(self):
+        self.order_total = self.order.aggregate(Sum('order_total'))['order_total_total__sum'] or 0
+
+    def __str__(self):
+        return self.order_number
+
+
