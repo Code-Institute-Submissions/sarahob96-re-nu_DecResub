e6d3373f24247958598ace8dcc8745f04a5c69c0
sarahob96 <sarahobrien15@gmail.com>
2022-10-16 18:15:57 +0000

    begin creating the checkout view and add to urls

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import CheckoutForm
 
 # Create your views here.
+
+def order(request):
+    bag = request.session.get('bag', {})
+    
+    #messages error
+
+    checkout_form = CheckoutForm()
+    template = 'checkout/checkout.html'
+    context = { 
+        'checkout_form': checkout_form,
+}
+    return render(request, template, context)
+    
