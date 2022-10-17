de56905977f082640f66bc76c5682a69a97598e8
sarahob96 <sarahobrien15@gmail.com>
2022-10-17 18:22:01 +0000

    create stripe.js a + use stripes elements to render the card form to the view

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
+import json
 from django.shortcuts import render
 from .forms import CheckoutForm
 
+from django.conf import settings 
 # Create your views here.
 
 def order(request):
@@ -12,6 +14,7 @@ def order(request):
     template = 'checkout/checkout.html'
     context = { 
         'checkout_form': checkout_form,
-}
+        'stripe_publishable_key': 'pk_test_51LtcVaK0jrFOpBVy2hvz7S8hYibPrZ7UISeyMK9jQ1RBQVLZUxetcW5B2Z6AP8U1yhtjmL3Edsvhv1TvnsPlOImz00wiIjCmqk',
+        'client_secret': 'test client secret',      
+    }
     return render(request, template, context)
-    
