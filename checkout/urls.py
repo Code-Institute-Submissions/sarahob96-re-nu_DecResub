e1cfa7c8b73a11365e908222a583249115a9d2bb
sarahob96 <sarahobrien15@gmail.com>
2022-10-21 22:15:43 +0000

    Set up webhook handlers to map events to corresponding handler functions

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.order, name='checkout'),
     path('order_success/<order_number>', views.order_successful, name="order_successful"),
-
+    path('webhook/', webhook, name='webhook')
 ]
