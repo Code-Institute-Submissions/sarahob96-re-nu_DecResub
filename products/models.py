74b8778ff83ca5144242aea9ace8b00cb6f0a2ef
sarahob96 <sarahobrien15@gmail.com>
2022-10-22 15:28:38 +0000

    add clothing sizes to all products except the accessories category

--- products/models.py
+++ products/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200)
     sku = models.CharField(max_length=200)
+    sizes = models.BooleanField(default=False, null=True, blank=True)
     product_description = models.TextField()
     rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
