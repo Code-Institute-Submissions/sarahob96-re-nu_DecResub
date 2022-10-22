ad1cc2552c0e95aed194c06054ed715a32ae437d
sarahob96 <sarahobrien15@gmail.com>
2022-10-22 22:54:58 +0000

    render the profile form to the view excluding the user field

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Profile
+from .forms import ProfileForm
 
 # Create your views here.
 
 def user_profile(request):
 
     my_profile = get_object_or_404(Profile, user=request.user)
+
+    form = ProfileForm(instance=my_profile)
+    #orders = my_profile.orders.all()
     template = 'profiles/profiles.html'
     context = {
+        'form': form,
         'my_profile': my_profile,
+        #'orders': orders
     }
 
     return render(request, template, context)
