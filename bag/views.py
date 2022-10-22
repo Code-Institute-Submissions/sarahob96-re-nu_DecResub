46aee5d85cc097fb14f39e85e33510bb19b27c03
sarahob96 <sarahobrien15@gmail.com>
2022-10-22 13:33:15 +0000

    add messages block to include toasts on base.html

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
-
+from django.contrib import messages
 # Create your views here.
 
 
@@ -11,6 +11,7 @@ def shopping_bag(request):
 
 def add_product(request, product_id):
 
+    product = Product.objects.get(pk=product_id)
     qty = request.POST.get('qty')
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,6 +20,7 @@ def add_product(request, product_id):
         bag[product_id] = int(qty)
     else:
         bag[product_id] += int(qty)
+        messages.success(request, f'Added {product.name} to the bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
