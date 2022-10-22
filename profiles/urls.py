d1bd3c30ac2c8f99710619b823e112c0d672d49d
sarahob96 <sarahobrien15@gmail.com>
2022-10-22 21:31:15 +0000

    create profile.html and link it to the nav bar for user access

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  
+  path('', views.user_profile, name="profile")
 
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_profile(request):
+
+    template = 'profiles/profiles.html'
+    context = {}
+
+    return render(request, template, context)
