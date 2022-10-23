6b755a1158bbf2f3c8484f3421e9a3221f709763
sarahob96 <sarahobrien15@gmail.com>
2022-10-23 22:41:31 +0000

    add contact form to the contact page

--- information/views.py
+++ information/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 
 def contact(request):
 
+
+    if request.method == 'POST':
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        message_fields = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message,
+        }   
+
     return render(request, 'information/contact.html')
