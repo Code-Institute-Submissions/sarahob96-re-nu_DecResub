ac0e420c8d0968320b966b7f2d97b7b211a1732c
sarahob96 <sarahobrien15@gmail.com>
2022-11-04 21:47:17 +0000

    connect static/media files to AWS

--- re_nu_apparel/settings.py
+++ re_nu_apparel/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'reviews',
     'information',
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -187,6 +188,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    #Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "renu-apparel"
+    AWS_S3_REGION_NAME = "us-east-1"
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    #static/media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #override static and media urls in production
+    STATIC_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
 
 FREE_DELIVERY = 60
 STANDARD_DELIVERY = 10
