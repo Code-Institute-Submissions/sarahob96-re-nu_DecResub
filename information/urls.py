08066d1807dd0beca0f39a7713f655903f18ac98
sarahob96 <sarahobrien15@gmail.com>
2022-11-06 20:23:10 +0000

    add meta tags

--- information/urls.py
+++ information/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/contact', views.form_contact, name="form_contact"),
+    path('contact/', views.form_contact, name="form_contact"),
     path('contact-success', views.contact_success, name="contact-success"),
     path('renu-go', views.renu_go, name="renu-go"),
     path('', views.renu_form_review, name="renu-form"),
