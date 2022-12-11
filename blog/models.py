81c50a152979b6fe4b3003ab2f7916fc54ccc2c4
sarahob96 <sarahobrien15@gmail.com>
2022-12-11 12:37:59 +0000

    create a comment model for the blog section and print comments to the front end

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse 
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -12,3 +13,13 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog-view', args=(str(self.id)))
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Article, related_name="blog_comment", on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.post.title
